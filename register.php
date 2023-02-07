5f2416ae5f329b244468dd2e91f9cb5cdb931cd1
Jhil-Patel <jhil2409@gmail.com>
2023-02-07 12:30:56 +0530

    new changes

--- register.php
+++ register.php
@@ -1,22 +1,43 @@
 <?php
 
+// if(isset($_POST['sub']))
+// {
+// $conn=mysql_connect('localhost','root','','medtour');
+// if($conn)
+// {
+//     echo"connected";
+// }
+// $usr=$_POST['urs'];
+// $pas=$_POST['pas'];
+// $sql="INSERT INTO `Register_pr`(`Uname`, `Upass`) VALUES ('$usr','$pas')";
+// if(mysql_query($conn,$sql))
+// {
+//     echo"value entered";
+// }
+// else{
+//     echo"error";
+// }
+// }
+
+
 if(isset($_POST['sub']))
 {
-$conn=mysqli_connect('localhost','root','','medtour');
-if($conn)
-{
-    echo"connected";
-}
-$usr=$_POST['urs'];
-$pas=$_POST['pas'];
-$sql="INSERT INTO `register`(`Username`, `Password`) VALUES ('$usr','$pas')";
-if(mysqli_query($conn,$sql))
-{
-    echo"value entered";
-}
-else{
-    echo"error";
-}
+    $conn=mysqli_connect('localhost','root','','Medtour');
+    if(!$conn)
+    {
+        echo "Not connected";
+    }
+    $user=$_POST['urs'];
+    $pass=$_POST['pas'];
+    $sql="INSERT INTO `Register_pr`(`Uname`, `Upass`) VALUES ('$user','$pass')";
+    if(mysqli_query($conn,$sql))
+    {
+        echo "Value entered";
+    }
+    else
+    {
+        echo "Failed to enter value";
+    }
 }
 
 ?>
