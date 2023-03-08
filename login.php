e2c54e213185ee09f47633edf1c58ad9bb6a794d
Jhil-Patel <jhil2409@gmail.com>
2023-03-09 00:55:27 +0530

    9-3-23

--- login.php
+++ login.php
@@ -1,10 +1,69 @@
 <?php
-
 if(isset($_POST['insert']))
 {
-    $conn=mysqli_connect('localhost','root','','medtour');
-    $username=$_POST['uname'];
-    $password=$_POST['pass'];
+    $utype=$_POST['category'];
+    $uname=$_POST['nm'];
+    $upass=$_POST['pswd'];
+    echo $utype."</br>";
+    echo $uname,$upass."</br>";
+    
+    $con=mysql_connect('localhost','root','','medtour');
+    if($con)
+    {
+        echo "connection successful";
+        $sql = "SELECT `UType`,`UName`,`UPass` FROM `registration` WHERE `UName`='$uname'";
+  
+   $result = mysql_query($sql,$con);
+//   echo "Query Successful";
+  
 
+   while($row = mysql_fetch_assoc($result))
+   {    
+        $utyp=$row['UType'];
+       $name=$row['UName'];
+       $pass=$row['UPass'];
+   }  
+       echo $typ."</br>";
+   echo $name,$pass."</br>";
+   echo "I am checking data";   
+   
+   
+
+          if($utype==$typ)
+          {
+            if($uname==$name)
+            {
+                if($pass==$upass)
+                {
+                  $sql3="INSERT INTO `login` VALUES ('$typ','$name','$pass')";
+                  if(mysql_query($sql3,$con))
+                  {
+                   
+
+echo "<script>alert('login successfuly');</script>";
+  echo ("<script LANGUAGE='JavaScript'>window. location. href='home.html';</script>");
+}
+else 
+{
+    echo "<script>alert('Something is wrong pl login again');</script>";
+  echo ("<script LANGUAGE='JavaScript'>window. location. href='registration.html';</script>");
+}
+
+
+                  }
+                  else
+                  {
+                //    header("location:adminloginform.htm"); 
+                  }
+                }
+            }
+             
+           
+         
+                
+            mysql_close($con);
+            
+        
+    }
 }
 ?>
